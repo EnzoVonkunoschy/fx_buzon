fed444ed1a6b34d92e04a11bb3f0d24d71fadcc1
Luciano Quiroga <lucianoquiroa07@gmail.com>
2025-05-22 18:44:23 -0300

    primer commit

--- java/com/example/buzon/Mensaje.java
+++ java/com/example/buzon/Mensaje.java
@@ -1,5 +1,18 @@
 package com.example.buzon;
 
 public class Mensaje {
+    private String remitente;
+    private String destinatario;
+    private String mensaje;
 
+    public Mensaje(String remitente, String destinatario, String mensaje) {
+        this.remitente = remitente;
+        this.destinatario = destinatario;
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String toString() {
+        return "Mensaje [remitente=" + remitente + ", destinatario=" + destinatario + ", mensaje=" + mensaje + "]";
+    }
 }
