c2b087198007c6d0e1336d1ccea038f6d53add0a
Flavia <flaviaprogramaciontorres@gmail.com>
2025-05-27 17:35:09 -0300

    rama_Flavia

--- java/com/example/buzon/Mensaje.java
+++ java/com/example/buzon/Mensaje.java
@@ -1,5 +1,29 @@
 package com.example.buzon;
 
 public class Mensaje {
+    String remitente;
+    String emisor;
+    String texto;
 
+    public Mensaje( String remitente, String emisor, String texto){
+        this.remitente= remitente;
+        this.emisor= emisor;
+        this.texto= texto;
+    }
+
+    public String getRemitente() {
+        return remitente;
+    }
+
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+    @Override
+    public String toString(){
+        return "Remitente: "+ remitente + "\n" + "Emisor: " + emisor + "\n"+ "Texto: "+ texto;
+    }
 }
