ae06b3e69bca927c86a4fefc25fb1e12de5dad15
claracarmona720 <claritacarmona720@gmail.com>
2025-05-28 15:25:31 -0300

    Modificacion clara

--- java/com/example/buzon/Mensaje.java
+++ java/com/example/buzon/Mensaje.java
@@ -1,5 +1,18 @@
 package com.example.buzon;
 
 public class Mensaje {
+    private String remitente;
+    private String destinatario;
+    private String texto;
 
+    public Mensaje(String remitente, String destinatario, String texto) {
+        this.remitente = remitente;
+        this.destinatario = destinatario;
+        this.texto = texto;
+    }
+
+    @Override
+    public String toString() {
+        return "Remitente= " + remitente + ", Destinatario= " + destinatario + ", Texto= " + texto;
+    }
 }
