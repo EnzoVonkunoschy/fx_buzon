24d530de5a2a0c4e945aca5a7d1cf642c259b172
Alam <alamblanco18@gmail.com>
2025-05-27 22:16:42 -0300

    SegundoCommit

--- java/com/example/buzon/Mensaje.java
+++ java/com/example/buzon/Mensaje.java
@@ -1,5 +1,20 @@
 package com.example.buzon;
 
 public class Mensaje {
+    private String remitente;
+    private String destinatario;
+    private String texto;
 
+    public Mensaje(String remitente, String destinatario, String texto) {
+        this.remitente = remitente;
+        this.destinatario = destinatario;
+        this.texto = texto;
+    }
+    public Mensaje() {
+    }
+
+    @Override
+    public String toString() {
+        return this.remitente +" "+ this.destinatario +" "+ this.texto;
+    }
 }
