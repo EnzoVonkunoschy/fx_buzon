dc23ff12b898266157652bbd10543978a3bf89d7
Luciano Quiroga <lucianoquiroa07@gmail.com>
2025-06-03 19:29:22 -0300

    segundo commit con los requerimientos

--- java/com/example/buzon/Buzon.java
+++ java/com/example/buzon/Buzon.java
@@ -4,4 +4,34 @@ import java.util.ArrayList;
 
 public class Buzon {
     ArrayList<Mensaje> mensajes = new ArrayList<>();
+
+
+    public boolean hayMensajes(String destinatario){
+        for (Mensaje mensajes : mensajes){
+            if (mensajes.getDestinatario().equals(destinatario)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Mensaje> retirarMensajes(String destinatario){
+        ArrayList<Mensaje> mensajesARetirar = new ArrayList<>();
+        for (Mensaje mensajes : mensajes){
+            if (mensajes.getDestinatario().equals(destinatario)){
+                mensajesARetirar.add(mensajes);
+            }
+        }
+        return mensajesARetirar;
+    }
+
+    public void recibirMensaje(Mensaje mensaje){
+        mensajes.add(mensaje);
+    }
+
+    public void listarMensajes(){
+        for (Mensaje mensajes : mensajes){
+            System.out.println(mensajes);
+        }
+    }
 }
